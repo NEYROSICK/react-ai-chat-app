10b59e31e30460206ba13dcd9f0d2e6ddf87f324
NEYROSICK <dhatak24@gmail.com>
2024-05-27 01:12:13 +0300

    moved environmental variables to .env file

--- src/lib/firebase.jsx
+++ src/lib/firebase.jsx
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX_pNeNmFa79UwetPZ4fIAcgzoKb_R9rM",
-  authDomain: "react-chat-app-49630.firebaseapp.com",
-  projectId: "react-chat-app-49630",
-  storageBucket: "react-chat-app-49630.appspot.com",
-  messagingSenderId: "18835669123",
-  appId: "1:18835669123:web:410005e10b5e7657c55bb6",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
